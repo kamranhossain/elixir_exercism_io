e709474ccb0fef6e62c1fe2b6504bab709cbfb36
kamranhossain <kamran.hossain227@gmail.com>
2020-07-30 16:08:13 +0600

    solution of word-count problem with test

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,12 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.split([" ", "_", ",", "?", "!"])
+    # filter out only words
+    |> Enum.filter(&Regex.run(~r/[\w\d]+/, &1))
+    # make word count map, create or update map word input Map.update 4 arity function.
+    |> Enum.reduce(%{}, fn word, counts -> Map.update(counts, word, 1, &(&1 + 1)) end)
   end
 end
